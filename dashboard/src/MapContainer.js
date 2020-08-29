708f95afdaf059ef41a1b48eb794502f97872bf6
rishi2907 <dev.rishiagrawal@gmail.com>
2020-08-29 19:09:44 +0530

    added socket_io

--- dashboard/src/MapContainer.js
+++ dashboard/src/MapContainer.js
@@ -1,74 +1,94 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import socketIOClient from "socket.io-client";
 
-const MapContainer = () => {
+class MapContainer extends Component {
 
-    const locations = [
-        {
-            name: "Location 1",
-            location: {
-                lat: 41.3954,
-                lng: 2.162
-            },
-        },
-        {
-            name: "Location 2",
-            location: {
-                lat: 41.3917,
-                lng: 2.1649
-            },
-        },
-        {
-            name: "Location 3",
-            location: {
-                lat: 41.3773,
-                lng: 2.1585
-            },
-        },
-        {
-            name: "Location 4",
-            location: {
-                lat: 41.3797,
-                lng: 2.1682
-            },
-        },
-        {
-            name: "Location 5",
-            location: {
-                lat: 41.4055,
-                lng: 2.1915
-            },
-        }
-    ];
-
-
-    const mapStyles = {
-        height: "100vh",
-        width: "100%"
-    };
-
-    const defaultCenter = {
-        lat: 41.3851, lng: 2.1734
+    constructor() {
+        super();
+        this.state = {
+            response: false,
+            endpoint: "http://127.0.0.1:4001",
+            locations: [],
+        };
     }
 
-    return (
-        <LoadScript
-            googleMapsApiKey='AIzaSyDzCP5oMHWf1dNgcIdchURB1UP5t0mMt70'>
-            <GoogleMap
-                mapContainerStyle={mapStyles}
-                zoom={13}
-                center={defaultCenter}
-            >
-                {
-                    locations.map(item => {
-                        return (
-                            <Marker key={item.name} position={item.location} />
-                        )
-                    })
-                }
-            </GoogleMap>
-        </LoadScript>
-    )
+    componentDidMount() {
+        // we can dynamically update it by using socket.io
+        const { endpoint } = this.state;
+        const socket = socketIOClient(endpoint);
+        socket.on("FromAPI", data => (this.setState({ locations: data }), console.log(data)));
+        // const loc = [
+        //     {
+        //         name: "Location 1",
+        //         location: {
+        //             lat: 44.3954,
+        //             lng: 2.162
+        //         },
+        //     },
+        //     {
+        //         name: "Location 2",
+        //         location: {
+        //             lat: 41.3917,
+        //             lng: 2.1649
+        //         },
+        //     },
+        //     {
+        //         name: "Location 3",
+        //         location: {
+        //             lat: 41.3773,
+        //             lng: 2.1585
+        //         },
+        //     },
+        //     {
+        //         name: "Location 4",
+        //         location: {
+        //             lat: 41.3797,
+        //             lng: 2.1682
+        //         },
+        //     },
+        //     {
+        //         name: "Location 5",
+        //         location: {
+        //             lat: 41.4055,
+        //             lng: 2.1915
+        //         },
+        //     }
+        // ];
+        // this.setState({ locations: loc });
+    }
+
+    render() {
+        const mapStyles = {
+            height: "100vh",
+            width: "100%"
+        };
+
+        const defaultCenter = {
+            lat: 41.3851, lng: 2.1734
+        }
+
+        return (
+            <div>
+                <LoadScript
+                    googleMapsApiKey='AIzaSyDzCP5oMHWf1dNgcIdchURB1UP5t0mMt70'>
+                    <GoogleMap
+                        mapContainerStyle={mapStyles}
+                        zoom={13}
+                        center={defaultCenter}
+                    >
+                        {
+                            this.state.locations.map(item => {
+                                return (
+                                    <Marker key={item.name} position={item.location} />
+                                )
+                            })
+                        }
+                    </GoogleMap>
+                </LoadScript>
+            </div>
+        );
+    }
 }
 
 export default MapContainer;
